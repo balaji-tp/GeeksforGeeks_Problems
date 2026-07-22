609203690c2f6bb264bbc5a80ca1187d68ed0414
Balaji T P <balajitpkrr@gmail.com>
2026-07-22 23:08:41 +0530

    feat(codeforces): Sync Elephant and 3 others

--- Codeforces/strings/Way Too Long Words/python/solution.py
+++ Codeforces/strings/Way Too Long Words/python/solution.py
@@ -1,9 +1,6 @@
-
 n = int(input())
 for _ in range(n):
     w = input()
     wl = len(w)
-    if wl > 10:
-        print(f"{w[0]}{wl-2}{w[-1]}")
-    else:
-        print(w)
+    
+    print(f"{w[0]}{wl-2}{w[-1]}" if wl > 10 else w)
